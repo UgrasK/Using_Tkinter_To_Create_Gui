766c3842ab4b692b43435ed2a61cfca224566472
UgrasK <ugurkaraer1989@gmail.com>
2022-07-25 12:58:35 +0300

    created a window and label with tkinter.

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 import tkinter
 
+# window
+window = tkinter.Tk()
+window.title("My first GUI program")
+window.minsize(width=500, height=300)
+
+# label
+my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
