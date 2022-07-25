bc70c255bf60dc0eeb928b90566b9e94c78ddeae
UgrasK <ugurkaraer1989@gmail.com>
2022-07-25 16:34:58 +0300

    defined a function and called it on button "command" to change label text with input text.

--- main.py
+++ main.py
@@ -11,13 +11,19 @@ my_label.pack()
 
 
 # define a function and call it on button command to change label text
+# def button_clicked():
+#     my_label.config(text="button got clicked")
+
+# define a function and call it on button command to change label text with input text
 def button_clicked():
-    my_label.config(text="button got clicked")
+    my_label.config(text=input.get())
 
 button = Button(text="Click me ", command=button_clicked)
 button.pack()
 
-
+# define an input field with Entry
+input = Entry(width=10)
+input.pack()
 
 
 
