18268ac7ade246631f9b7662d3b9d84baaa596bf
UgrasK <ugurkaraer1989@gmail.com>
2022-07-25 17:01:11 +0300

    used place() instead of pack() to set exact position with x,y values.

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ window.minsize(width=500, height=300)
 
 # label
 my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack()
+# my_label.pack()
+my_label.place(x=20, y=0)
+
 
 
 # define a function and call it on button command to change label text
@@ -19,11 +21,13 @@ def button_clicked():
     my_label.config(text=input.get())
 
 button = Button(text="Click me ", command=button_clicked)
-button.pack()
+# button.pack()
+button.place(x=20, y=50)
 
 # define an input field with Entry
 input = Entry(width=10)
-input.pack()
+# input.pack()
+input.place(x=20, y=100)
 
 
 
