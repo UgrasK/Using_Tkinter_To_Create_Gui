3cd67d9460902a9a124bb27b7fb774dcf3ea93fd
UgrasK <ugurkaraer1989@gmail.com>
2022-07-25 16:08:09 +0300

    defining function with unlimited keyword arguments with "**".

--- playground.py
+++ playground.py
@@ -7,3 +7,14 @@ def add(*args):
 
 
 print(add(3, 5, 6, 8))
+
+# define unlimited keyword arguments on function with **kwargs (kwargs can be any name)
+class Car:
+
+    def __init__(self, **kw):
+        self.brand = kw.get("brand")
+        self.model = kw.get("GT-R")
+
+
+my_car = Car(brand="Porsche")
+print(my_car.brand)
